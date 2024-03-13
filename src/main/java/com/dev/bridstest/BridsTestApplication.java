895cdca273baa7d574987f5df8fcde1d45ef19a7
csnnidhom <chusnunnidhom@gmail.com>
2024-03-13 11:53:45 +0700

    soal 1

---
+++ src/main/java/com/dev/bridstest/BridsTestApplication.java
@@ -0,0 +1,21 @@
+package com.dev.bridstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridsTestApplication.class, args);
+
+		System.out.println("===>>> Soal 1 <<<===");
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/test/java/com/dev/bridstest/BridsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.bridstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
